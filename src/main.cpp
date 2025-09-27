4bcf71faade6e5ef61a6c8052cb484a87f22e3ca
Ayush Soni <soni.ayush.2212@gmail.com>
2025-09-27 21:53:32 +0530

    Add: combobox and event handling to send dummy message

--- src/main.cpp
+++ src/main.cpp
@@ -9,62 +9,74 @@ public:
   virtual bool OnInit();
 };
 
-class MyFrame: public wxFrame {
+class MyFrame : public wxFrame {
 public:
-    MyFrame();
+  MyFrame();
 
 private:
-    void OnExit(wxCommandEvent& event);
-    void OnClick(wxCommandEvent& event);
-    wxTextCtrl* m_textCtrl;
+  void OnExit(wxCommandEvent &event);
+  void OnSend(wxCommandEvent &event);
+  wxTextCtrl *myTextCtrl;
+  wxComboBox *myComboBox;
 };
 
-enum {ID_Hello = 1};
+enum { ID_Hello = 1 };
 
 wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit() {
-    MyFrame* frame = new MyFrame();
-    frame->Show(true);
-    return true;
+  MyFrame *frame = new MyFrame();
+  frame->Show(true);
+  return true;
 }
 
-MyFrame::MyFrame(): wxFrame(NULL, wxID_ANY, "TinyClient - A really tiny REST Client") {
-    wxPanel* panel = new wxPanel(this, wxID_ANY);
+MyFrame::MyFrame()
+    : wxFrame(NULL, wxID_ANY, "TinyClient - A really tiny REST Client") {
 
-    wxBoxSizer* hbox = new wxBoxSizer(wxVERTICAL);
+  wxPanel *panel = new wxPanel(this, wxID_ANY);
+  wxBoxSizer *vbox = new wxBoxSizer(wxVERTICAL);
+  wxBoxSizer *hbox = new wxBoxSizer(wxHORIZONTAL);
 
-    m_textCtrl = new wxTextCtrl(panel, wxID_ANY, "", wxDefaultPosition, wxSize(10, -1));
+  // Combo Box
+  wxString choices[] = {"GET", "POST"};
+  myComboBox =
+      new wxComboBox(panel, wxID_ANY, choices[0], wxPoint(10, 10),
+                     wxSize(80, -1), WXSIZEOF(choices), choices, wxCB_READONLY);
 
-    wxButton* button = new wxButton(panel, wxID_ANY, "Get Text");
+  // Text Control
+  myTextCtrl = new wxTextCtrl(panel, wxID_ANY, "http://", wxPoint(10, 50),
+                              wxSize(200, -1));
 
-    button->Bind(wxEVT_BUTTON, &MyFrame::OnClick, this);
+  // Button
+  wxButton *myButton = new wxButton(panel, wxID_ANY, "Send");
+  myButton->Bind(wxEVT_BUTTON, &MyFrame::OnSend, this);
 
-    hbox->Add(m_textCtrl, 1, wxEXPAND | wxALL, 5);
-    hbox->Add(button, 0, wxALL, 5);
+  hbox->Add(myComboBox, 0, wxALL, 2);
+  hbox->Add(myTextCtrl, 0, wxALL, 2);
 
-    panel->SetSizer(hbox);
+  vbox->Add(hbox, 0, wxALL, 2);
+  vbox->Add(myButton, 0, wxALL, 2);
 
-    wxMenu* menuFile = new wxMenu;
-    menuFile->Append(wxID_EXIT);
+  panel->SetSizer(vbox);
 
-    wxMenuBar* menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
+  wxMenu *menuFile = new wxMenu;
+  menuFile->Append(wxID_EXIT);
+  wxMenuBar *menuBar = new wxMenuBar;
+  menuBar->Append(menuFile, "&File");
+  SetMenuBar(menuBar);
 
-    SetMenuBar(menuBar);
+  CreateStatusBar();
+  SetStatusText("TinyClient");
 
-    CreateStatusBar();
-    SetStatusText("TinyClient");
-
-    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+  Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
 };
 
-void MyFrame::OnExit(wxCommandEvent& event) {
-    Close(true);
-};
+void MyFrame::OnExit(wxCommandEvent &event) { Close(true); };
 
-void MyFrame::OnClick(wxCommandEvent& event) {
-    wxString text = m_textCtrl->GetValue();
-
-    wxMessageBox("The text is the box is: \n" + text, "Text from box", wxOK | wxICON_INFORMATION);
-};
+void MyFrame::OnSend(wxCommandEvent &event) {
+  wxString method = myComboBox->GetValue();
+  wxString url = myTextCtrl->GetValue();
+  
+  wxMessageBox("Sending " + method + " request to " + url, "Request Send",
+               wxOK | wxICON_INFORMATION);
+}
